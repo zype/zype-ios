88dfd73e892bd44d1ccb4de948c7077c149ea1d2
Andrey Kasatkin <a@svetliy.com>
2017-04-03 10:53:09 -0400

    dont load highlights

--- Zype/AppDelegate.m
+++ Zype/AppDelegate.m
@@ -77,7 +77,7 @@
 - (void)applicationDidBecomeActive:(UIApplication *)application {
     // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     
-    [ACSDataManager syncHighlights];
+    //[ACSDataManager syncHighlights];
     
     // Register for notification
     [self registerForNotification];
