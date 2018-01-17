db3249aae8f0af1844aa1194db553217231693d4
Александр <buga-91@inbox.ru>
2018-01-17 17:58:29 +0600

    configure key kAppAppleTVLayoutShowThumbanailTitle added

--- Zype/Utilities/ZypeCommon.h
+++ Zype/Utilities/ZypeCommon.h
@@ -27,6 +27,7 @@
 
 #define kAppColorLight NO
 #define kAppAppleTVLayout YES
+#define kAppAppleTVLayoutShowThumbanailTitle NO
 
 #define kClientColor    [UIUtil colorWithHex:0xF75532];
 #define kTextPlaceholderColor [UIColor colorWithRed:0.72 green:0.72 blue:0.75 alpha:1.00]
