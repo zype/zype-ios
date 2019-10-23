739e55f15faa55f1f0b129ce1c080f1f2f3f4fa4
Christian Nicholson <advantiss@outlook.com>
2019-10-23 10:44:17 -0700

    update for apps-485

--- Zype/Utilities/ZypeCommon.h
+++ Zype/Utilities/ZypeCommon.h
@@ -77,6 +77,10 @@
 // Enable to display playlist and video titles underneath each thumbnail image
 #define kInlineTitleTextDisplay NO
 
+// Enable to add live menu item
+#define kLiveItemEnabled NO
+#define kLiveVideoID @"5c8faa021d1f4314dd006203"
+
 #define kClientColor    [UIUtil colorWithHex:0xF75532];
 #define kTextPlaceholderColor [UIColor colorWithRed:0.72 green:0.72 blue:0.75 alpha:1.00]
 #define kLightTintColor [UIColor colorWithRed:1.00 green:0.11 blue:0.38 alpha:1.00]
