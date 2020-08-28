9e9b0a95ab249a8fac634acdd4e553a3e4a4b998
Anish <anishioscoder@gmail.com>
2020-08-28 22:15:21 +0530

    Fix for swift auto generated header issue and 1 minor compilation issue

--- Zype/Utilities/ZypeCommon.h
+++ Zype/Utilities/ZypeCommon.h
@@ -96,7 +96,7 @@
 #define kSegmentAccountName @"enter_account_name_here"
 
 
-#define kClientColor    [UIUtil colorWithHex:0xF75532];
+#define kClientColor    [UIUtil colorWithHex:0xF75532]
 #define kTextPlaceholderColor [UIColor colorWithRed:0.72 green:0.72 blue:0.75 alpha:1.00]
 #define kLightTintColor [UIColor colorWithRed:1.00 green:0.11 blue:0.38 alpha:1.00]
 #define kLightLineColor [UIColor colorWithRed:0.70 green:0.70 blue:0.70 alpha:1.00]
